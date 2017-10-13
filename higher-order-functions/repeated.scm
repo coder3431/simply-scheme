f6f57bdd3d0301252db8867ce0d544c4e2e75e1b
steve <steve@localhost.localdomain>
2017-10-13 01:25:39 -0400

    Add repeated.scm

---
+++ higher-order-functions/repeated.scm
@@ -0,0 +1,21 @@
+; repeated take a function and executes itself n times.
+;           
+; arguments: f -> any function
+;            n -> number of times 
+; 
+; output:    returns a function that takes any argument
+; (f (f (f x)))
+
+(define (repeated f n)
+    (lambda(x)
+        (define (loop n)
+              (if (=  n 1)
+                (f x)
+                (f (loop (- n 1)))))
+
+        (if (= n 0)
+           x 
+          (loop n))))
+
+
+
