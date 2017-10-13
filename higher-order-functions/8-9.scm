8e48679e6ab20d01dd4d8c43edd7aa50dbe800c9
steve <steve@localhost.localdomain>
2017-10-13 19:13:57 -0400

    Add 8-9.scm

---
+++ higher-order-functions/8-9.scm
@@ -0,0 +1,8 @@
+Excerise 8.9
+; What procedure can you use as the first argument to every so that for any sentence used as the second argument, every returns that sentence?  sentence 
+
+; What procedure can you as the first argument to keep so it returns a sentence?
+(keep sentence? sentence)  trick question.. a sentence on a sentnce is false.  should be word.
+
+; What procedure can you use as first argument to accumulate?
+(accumulate _____ '(hello world') sentence
